b1ab5ac7ff78d57fe84c336c447452a3f6b3f600
amritkundan <66768804+amritkundan@users.noreply.github.com>
2023-04-26 20:23:07 -0400

    Update guessanumber4.js

--- guessanumber4.js
+++ guessanumber4.js
@@ -1,45 +1,44 @@
-// HelloWorld1.js - a simple API running on Node.js and using Express
-const express = require('express');
-const app = express();
-app.use(express.urlencoded({extended: false}));
+var express = require('express');
+var app = express();
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-let randomNumber = [];
+var randomNumber = [];
 
 app.use(function(req, res, next) {
-  express.urlencoded({extended: false});
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  express.urlencoded({ extended: false });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-app.post('/startGame', function(req,res){
-  const gameId = req.body.gameId;
-  const randomNumberGenerated = Math.floor(Math.random() * 100) + 1; 
-  randomNumber[gameId] = randomNumberGenerated;
-  console.log(`Creating game number ${gameId}. The number to guess is ${randomNumberGenerated}`);
-  responseMessage = { APIMessage: new String(`Game number ${gameId} has started`) };
+app.post('/startGame', function(req, res) {
+  var randomNumberGenerated = Math.floor(Math.random() * 100) + 1;
+  var gameNum = req.body.gameId;
+  console.log(`Creating game number ${gameNum}. The number to guess is ${randomNumberGenerated}.`);
+  randomNumber[gameNum] = randomNumberGenerated;
+  var responseMessage = { APIMessage: `Game number ${req.body.gameId} has started.` };
   res.json(responseMessage);
 });
 
-app.get('/guessMade', function(req,res){
-  const gameId = req.query.gameId;
-  const numberToGuess = randomNumber[gameId];
-  const numberGuessed = req.query.guessMade;
-  let responseMessage = "";
-    
+app.get('/guessMade', function(req, res) {
+  var numberToGuess = randomNumber[req.query.gameId];
+  var numberGuessed = req.query.guessMade;
+  var responseMessage = "";
+  var remainingGuesses = 5 - req.query.numGuesses;
+
   if (numberGuessed < numberToGuess) {
-    responseMessage = { APIMessage: new String('Your guess is too low - try again') };
+    responseMessage = `Your guess is too low - try again. ${remainingGuesses} guesses left.`;
   } else if (numberGuessed > numberToGuess) {
-    responseMessage = { APIMessage: new String('Your guess is too high - try again') };
+    responseMessage = `Your guess is too high - try again. ${remainingGuesses} guesses left.`;
   } else {
-    responseMessage = { APIMessage: new String('Your guess is correct!') };
+    responseMessage = "Congratulations! You guessed the correct number.";
   }
 
-  console.log(`Game number ${gameId} guessed ${numberGuessed}. The number to guess is ${numberToGuess}.`);
-  res.json(responseMessage);
+  console.log(`Game number ${req.query.gameId} guessed ${numberGuessed}. The number to guess is ${numberToGuess}.`);
+  res.json({ APIMessage: responseMessage });
 });
 
-console.log('Listening on port 8080');
+console.log("Listening on port 8080");
 app.listen(8080);
